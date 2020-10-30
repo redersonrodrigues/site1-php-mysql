2136b57b6a7e4ae5fc73496fc2c7715d29992a81
Rederson Rodrigues <r.ataliba@hotmail.com>
2020-10-30 19:54:20 -0200

    Criada área para divulgação de serviços

--- application/site/controllers/Home.php
+++ application/site/controllers/Home.php
@@ -21,6 +21,9 @@ class Home {
         $listar_car = new \Site\models\SiteCarousel();
         $this->Dados['sts_carousels'] = $listar_car->listar();
         
+        $listar_ser = new \Site\models\SiteServico();
+        $this->Dados['sts_servicos'] = $listar_ser->listar();
+        
         $carregarView = new \Core\ConfigView("/site/views/home/home", $this->Dados);
         $carregarView->renderizar();
     }
