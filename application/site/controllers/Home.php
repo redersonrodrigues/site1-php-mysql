d0625bbe531ca13f57a8efc2d5a46add30e96c4c
Rederson Rodrigues <r.ataliba@hotmail.com>
2020-10-30 19:04:52 -0200

    Inserir botões e comentarios no carousel

--- application/site/controllers/Home.php
+++ application/site/controllers/Home.php
@@ -20,12 +20,15 @@ class Home {
         //echo 'Página HOME (inicial) <br>';
         $listar_car = new \Site\models\SiteCarousel();
         $this->Dados['sts_carousels'] = $listar_car->listar();
+<<<<<<< HEAD
         
         $listar_ser = new \Site\models\SiteServico();
         $this->Dados['sts_servicos'] = $listar_ser->listar();
         
         $listar_vid = new \Site\models\SiteVideo();
         $this->Dados['sts_videos'] = $listar_vid->listar();
+=======
+>>>>>>> 7a49bd5... Inserir botões e comentarios no carousel
         
         $carregarView = new \Core\ConfigView("/site/views/home/home", $this->Dados);
         $carregarView->renderizar();
